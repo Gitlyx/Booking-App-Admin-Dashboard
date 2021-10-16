0d202e18a7990b45fda6fc721ea6d938287612fb
Charlie Vo <charlievo22@gmail.com>
2021-10-16 19:58:07 +0200

    s

--- ReactApplication/ClientApp/src/components/Login.js
+++ ReactApplication/ClientApp/src/components/Login.js
@@ -1,11 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Login extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Login page - Must be updated</h1>
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: "",
+      password: "",
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    console.log(this.stats.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form>
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+              <input type="text" />
             </div>
-        );
-    }
+            <div className="col-6">
+              <input type="text" placeholder />
+            </div>
+
+          </div>
+        </div>
+      </form>
+    );
+  }
 }
