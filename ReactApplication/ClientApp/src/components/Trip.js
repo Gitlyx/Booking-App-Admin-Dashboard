783749254c805e736dfbd62fd991353ba3356381
MinhTT43 <72129577+MinhTT43@users.noreply.github.com>
2021-10-21 21:32:41 +0200

    Recommit

--- ReactApplication/ClientApp/src/components/Trip.js
+++ ReactApplication/ClientApp/src/components/Trip.js
@@ -1,19 +1,27 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 export const Trip = () => {
   // ----- State -----
-  const [isToggled, setToggled] = useState("Hello");
+  const [printRute, setPrintRute] = useState({rute:[]})
 
-  // ----- Functions -----
-  const changeHandler = (e) => {
-    setToggled(e.target.value);
-    console.log(isToggled)
-  }
+  // ----- Function -----
 
-
+  
   return (
     <>
-        
+      <div className={"d-flex flex-row"}>
+        <h1>
+          Oversikt{" "}
+          <Link to="newroute">
+            <i className={"bi bi-plus-square px-3"} />
+          </Link>
+        </h1>
+        <div>
+          {/* {printRute.rute.map(rute => <div>rute.ruteFra</div>)} */}
+        </div>
+      </div>
     </>
   );
 };
